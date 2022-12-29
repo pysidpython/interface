7580a702c8ad84c1dae8aa748c9949a0f574fa73
Nycolas Ramires <nycolasramires2000@gmail.com>
2022-12-29 14:24:56 -0300

    Add comunication, plot and solve.py

---
+++ plot.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from pysid.identification.models import polymodel
+from pysid.io.csv_data import gen_data
+import matplotlib.pyplot as plt
+
+def plot(model,u,y):
+    # print(model.A,"\n",model.A[0],"\n",model.A[0,0],"\n",type(model.A))
+    y_m = gen_data(model.A[0,0], model.B[0,0], u.shape[0], u, 0)[:,1]
+    plt.title("Saída Real / Saída Estimada", fontsize=20)
+    plt.plot(range(len(u)),u,'.-',label='Entrada',lw=1.5)
+    plt.plot(range(len(y)),y,'.-',label='Saída Real',lw=2)
+    plt.plot(range(len(y_m)),y_m,'-*',label='Saída Estimada',lw=2)
+    plt.grid()
+    plt.legend(fontsize=10)
+    plt.show()
+    
