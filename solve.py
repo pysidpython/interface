b36c41d57bb1a0f447c651d2cdccbf640debaac5
Nycolas Ramires <nycolasramires2000@gmail.com>
2023-02-23 22:52:15 -0300

    changed the names to english(mq -> ls) and removed commented code

--- solve.py
+++ solve.py
@@ -1,14 +1,13 @@
 
-
 from pysid.io.print import print_model
 from pysid.identification.models import polymodel
-from pysid.identification.pemethod import arx, emq
-from pysid.identification.recursive import mqr
+from pysid.identification.pemethod import arx, els
+from pysid.identification.recursive import rls
 from pysid.io.csv_data import gen_data
 from numpy.linalg import inv
 from numpy import matmul,concatenate,ones,power
 
-def mq_interface(na,nb,nk,u,y,prec=3):
+def ls_interface(na,nb,nk,u,y,prec=3):
     
     ny = y.shape[1]
     nu = u.shape[1]
@@ -25,25 +24,19 @@ def mq_interface(na,nb,nk,u,y,prec=3):
     return m
 
 
-def mqe_interface(na,nb,nc,nk,u,y,th,n_max,prec):
-    m = emq(na, nb, nc, nk, u, y, th, n_max)
+def els_interface(na,nb,nc,nk,u,y,th,n_max,prec):
+    m = els(na, nb, nc, nk, u, y, th, n_max)
     print_model(m,prec=prec)
-    # y_m = gen_data(m.A[0,0], m.B[0,0], u.shape[0], u, 0)[:,1]
-    # print(y_m[:15])
-    # print(y[:15])
-    # soma = power((y-y_m),2)
-    # soma = soma.sum()/y.shape[0]
-    
-    # print(soma.max())
-    # print(soma)
-    # print(f'Cost function per sample: {soma:.7f}')
+   
+    # TODO : print(f'Cost function per sample: ...')
     # print("Infos de saída(matriz de covariancia) e tal")
     # print("Infos de regressão(n de amostras, sis mimo..)\n")
     return m
 
-def mqr_interface(na,nb,nk,u,y,prec):
-    m = mqr(na,nb,nk,u,y)
+def els_interface(na,nb,nk,u,y,prec):
+    m = rls(na,nb,nk,u,y)
     print_model(m,prec=prec)
+    # TODO : print(f'Cost function per sample: ...')
     # print("Infos de saída(matriz de covariancia) e tal")
     # print("Infos de regressão(n de amostras, sis mimo..)\n")
     return m
